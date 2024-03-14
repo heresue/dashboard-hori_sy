163d9b84a80be1628bca3856a67b96d1c5e704dd
heresue <sue.synpark@gmail.com>
2024-03-14 13:13:40 +0900

    useEffect 배경색변경(home)

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -16,10 +16,46 @@ import YourPie from "./components/YourPie";
 import ComplexTable from "./components/ComplexTable";
 import Task from "./components/Task";
 import MiniCalendar from "../../components/calendar/MiniCalendar";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  // useEffect(함수, [의존성 배열])
+  // *의존성 배열: package.json 열어보면 dependencies라는 의존성 배열들이 있음. 이와같은 역할
+
+  // 1. 컴포넌트가 언마운트 될 때(소멸) -----------------------------
+  // --------------------------------------------------------------
+  const [color, setColor] = useState("lightblue");
+
+  useEffect(() => {
+    // 1. 컴포넌트가 마운트 될 때(탄생) -----------------------------
+    //  - 배경색을 노란색으로 변경
+    //  탄생이니까 무조건 렌더링되기 때문에 의존할 게 없음. 의존성배열은 빈 상태로 둔다.
+    document.body.style.backgroundColor = color;
+    document.body.id = "intro"; // id값 셋팅 (id값을 넣어줌)
+    document.body.classList.add("intro"); // class 네임 셋팅
+
+    // 2. 컴포넌트가 언마운트 될 때(소멸) -----------------------------
+    return () => {
+      document.body.style.backgroundColor = "white";
+      document.body.id = "";
+      document.body.classList.remove("intro");
+    };
+    // --------------------------------------------------------------
+  }, [color]);
+
+  // 3. 업데이트: 버튼 클릭시 배경색 변경 ----------------------------
+  useEffect(() => {
+    console.log(`색상이 ${color}로 변경되었습니다.`);
+  }, [color]);
+
+  const toggleColor = () => {
+    setColor(color === "lightblue" ? "yellow" : "lightblue");
+  };
+  // --------------------------------------------------------------
+
   return (
     <VStack spacing={5}>
+      <button onClick={toggleColor}>배경색 변경!</button>
       <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing="20px" w={"100%"}>
         <MiniSt
           name="Earnings"
@@ -97,8 +133,8 @@ const Home = () => {
       <SimpleGrid columns={{ base: 1, md: 2 }} spacing="20px" w={"100%"}>
         <ComplexTable />
         <SimpleGrid columns={{ base: 1, md: 2 }} spacing="20px" w={"100%"}>
-          <Task/>
-          <MiniCalendar/>
+          <Task />
+          <MiniCalendar />
         </SimpleGrid>
       </SimpleGrid>
     </VStack>
